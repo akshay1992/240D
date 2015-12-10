6dce9b31e7c782e2677c33e4343bbcd677d3912f
akshay1992 <akshay19.92@gmail.com>
2015-12-10 15:25:12 -0800

    Modified App to use Headset audioscene example.

--- final/App.cpp
+++ final/App.cpp
@@ -1,6 +1,14 @@
 #include "allocore/al_Allocore.hpp"
 #include "Gamma/SoundFile.h"
 
+#include "allocore/sound/al_AudioScene.hpp"
+#include "allocore/sound/al_Vbap.hpp"
+#include "allocore/sound/al_Speaker.hpp"
+#include "alloutil/al_AlloSphereSpeakerLayout.hpp"
+
+#define BLOCK_SIZE 128
+#define SAMPLE_RATE 44100
+
 #include "TestSignals.hpp"
 #include "SpatialEcho.hpp"
 
@@ -12,10 +20,19 @@ class MyApp : public al::App, public PositionField
 {
 public:
     SpatialEcho* echo;
+    al::SpeakerLayout speakerLayout = al::HeadsetSpeakerLayout();
+    al::StereoPanner* panner = new al::StereoPanner(speakerLayout);
+    al::Listener* listener;
+
+    al::AudioScene* scene = new al::AudioScene(BLOCK_SIZE);
 
     declareTestSig();
     MyApp()
     {
+        listener = scene->createListener(panner);
+        panner->compile(*listener);
+
+
         echo = new SpatialEcho(0.04, 0.3);
 
         sf.format(gam::SoundFile::WAV);
@@ -27,10 +44,17 @@ public:
         gam::Domain::master().spu(44100);
 
         setupTestSig(1);
-        initAudio(44100, 128, 2, 0);
+        initAudio(SAMPLE_RATE, BLOCK_SIZE, 2, 0);
 
         al::Vec3f focus(toCartesian(1, 60, 0));
         setBounds(focus, 0.01);
+
+        std::vector<al::SoundSource*> sources = echo->get_sources();
+        for(int i=0; i<echo->get_nchannels(); i++)
+            scene->addSource(*sources[i]);
+
+        listener->compile();
+
 
     }
 
@@ -53,11 +77,13 @@ public:
             io.out(1) = out[1];
 
             sf.write(y, 1);
+            scene->render(io);
         }
     }
 };
 
 int main(){
     MyApp().start();
+    sf.close();
 }
 
